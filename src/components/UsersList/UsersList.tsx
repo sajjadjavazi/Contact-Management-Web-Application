2da944b2d4a052d5541b9146f4f9557e098031ad
Sajjad <sajjad.javazi.01@gmail.com>
2024-08-05 20:18:54 +0330

    feat(UserList): User list created

---
+++ src/components/UsersList/UsersList.tsx
@@ -0,0 +1,14 @@
+import UserCart from "./UserCart/UserCart";
+
+const UsersList = () => {
+  return (
+    <div className="flex justify-center w-1/2 flex-wrap">
+      <h1 className="font-bold text-xl mb-4 mt-4">لیست کاربران</h1>
+      <div className="bg-slate-100 w-11/12 h-80 rounded-lg mb-6">
+      <UserCart />
+      </div>
+    </div>
+  );
+};
+
+export default UsersList;
