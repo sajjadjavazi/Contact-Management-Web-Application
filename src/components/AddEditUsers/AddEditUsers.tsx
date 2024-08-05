80b4460c35e88b35a60f2cdde12ccce498ab74e6
Sajjad <sajjad.javazi.01@gmail.com>
2024-08-05 20:19:48 +0330

    feat(AddEditUser): Add / Edit user section created

---
+++ src/components/AddEditUsers/AddEditUsers.tsx
@@ -0,0 +1,12 @@
+const AddEditUsers = () => {
+  return (
+    <div className="flex justify-center w-1/2 flex-wrap">
+      <h1 className="font-bold text-xl mb-4 mt-4">اضافه / ویرایش کاربران</h1>
+        <div className="bg-slate-100 w-11/12 h-80 rounded-lg mb-6">
+
+        </div>
+    </div>
+  );
+};
+
+export default AddEditUsers;
