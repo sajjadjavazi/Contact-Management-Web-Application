845b5f5d4772dde8f1379232169244d86277796c
Sajjad <sajjad.javazi.01@gmail.com>
2024-08-05 20:20:57 +0330

    feat(app): layout and components imported.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import AddEditUsers from "./components/AddEditUsers/AddEditUsers";
+import UsersList from "./components/UsersList/UsersList";
+import Layout from "./layout/Layout";
 
 function App() {
   return (
-    <>
-    </>
-  )
+    <Layout>
+      <div className="flex justify-between">
+        <AddEditUsers />
+        <UsersList />
+      </div>
+    </Layout>
+  );
 }
 
-export default App
+export default App;
