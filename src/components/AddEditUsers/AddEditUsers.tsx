bdca932be318d330e45bbad0ece02838ea891fec
Sajjad <sajjad.javazi.01@gmail.com>
2024-08-05 20:58:21 +0330

    feat(AddEditUsers): inputs created

--- src/components/AddEditUsers/AddEditUsers.tsx
+++ src/components/AddEditUsers/AddEditUsers.tsx
@@ -2,9 +2,44 @@ const AddEditUsers = () => {
   return (
     <div className="flex justify-center w-1/2 flex-wrap">
       <h1 className="font-bold text-xl mb-4 mt-4">اضافه / ویرایش کاربران</h1>
-        <div className="bg-slate-100 w-11/12 h-80 rounded-lg mb-6">
-
+      <div className="bg-slate-100 w-11/12 rounded-lg mb-6 py-3">
+        <div className="mx-4">
+          <label htmlFor="#">نام:</label>
+          <input
+            type="text"
+            className="w-full h-10 border mt-1 rounded px-4  bg-gray-50"
+          />
         </div>
+        <div className="mx-4">
+          <label htmlFor="#">نام خانوادگی:</label>
+          <input
+            type="text"
+            className="w-full h-10 border mt-1 rounded px-4  bg-gray-50"
+          />
+        </div>
+        <div className="mx-4">
+          <label htmlFor="#">شماره موبایل:</label>
+          <input
+            type="text"
+            className="w-full h-10 border mt-1 rounded px-4  bg-gray-50"
+          />
+        </div>
+        <div className="mx-4">
+          <label htmlFor="#">نسبت:</label>
+          <input
+            type="text"
+            className="w-full h-10 border mt-1 rounded px-4  bg-gray-50"
+          />
+        </div>
+        <div className="mx-4">
+          <label htmlFor="#">ایمیل:</label>
+          <input
+            type="text"
+            className="w-full h-10 border mt-1 rounded px-4  bg-gray-50"
+          />
+        </div>
+        <button className="bg-green-500 px-6 py-3">اضافه کردن</button>
+      </div>
     </div>
   );
 };
