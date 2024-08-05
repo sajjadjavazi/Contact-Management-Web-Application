18af6c82cc5a34d92655c690e1a49883bf0a3699
Sajjad <sajjad.javazi.01@gmail.com>
2024-08-05 20:18:13 +0330

    feat(cart): cart created

--- src/components/UsersList/UserCart/UserCart.tsx
+++ src/components/UsersList/UserCart/UserCart.tsx
@@ -1,5 +1,16 @@
 const UserCart = () => {
-  return <div></div>;
+  return (
+    <div className="bg-slate-300 w-5/12 m-4 rounded-xl py-5 px-8">
+      <div>نام:</div>
+      <div>شماره تماس:</div>
+      <div>نسبت:</div>
+      <div>ایمیل:</div>
+      <div className="flex justify-end mt-4">
+        <button className="bg-blue-500 py-2 px-4 rounded-tr-md rounded-br-md">ویرایش</button>
+        <button className="bg-red-500 py-2 px-4 rounded-tl-md rounded-bl-md">حذف</button>
+      </div>
+    </div>
+  );
 };
 
 export default UserCart;
