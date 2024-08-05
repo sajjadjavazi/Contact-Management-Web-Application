10c8502bc4bcea11cfae539910f36d85459b0783
Sajjad <sajjad.javazi.01@gmail.com>
2024-08-05 19:30:55 +0330

    feat(footer): footer created

---
+++ src/layout/Footer/Footer.tsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return (
+      <footer className="bg-orange-500 w-full h-10 flex justify-center items-center">
+        Made by hardwork and love - Sajjad
+      </footer>
+    );
+  };
+  
+  export default Footer;
+  
