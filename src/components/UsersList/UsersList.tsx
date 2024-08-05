1ec833dbfe5496a014e7e4e018cbd49e4482ea06
Sajjad <sajjad.javazi.01@gmail.com>
2024-08-05 20:57:15 +0330

    fix(UsersList): display fixed

--- src/components/UsersList/UsersList.tsx
+++ src/components/UsersList/UsersList.tsx
@@ -2,9 +2,9 @@ import UserCart from "./UserCart/UserCart";
 
 const UsersList = () => {
   return (
-    <div className="flex justify-center w-1/2 flex-wrap">
+    <div className="flex justify-center w-1/2 flex-wrap content-start">
       <h1 className="font-bold text-xl mb-4 mt-4">لیست کاربران</h1>
-      <div className="bg-slate-100 w-11/12 h-80 rounded-lg mb-6">
+      <div className="bg-slate-100 w-11/12 rounded-lg mb-6">
       <UserCart />
       </div>
     </div>
