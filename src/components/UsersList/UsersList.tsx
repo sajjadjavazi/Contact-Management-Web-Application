4ddbf78ad26abf147d3434ce6955167a72f2893f
sajjadjavazi <sajjad.javazi.01@gmail.com>
2024-08-06 19:29:10 +0330

    feat(UsersList): props passed to elements.

--- src/components/UsersList/UsersList.tsx
+++ src/components/UsersList/UsersList.tsx
@@ -3,13 +3,13 @@ import UserCart from "./UserCart/UserCart";
 
 const UsersList = () => {
   return (
-    <div className="flex justify-center w-1/2 flex-wrap content-start">
+    <div className="flex justify-center w-1/2 flex-wrap content-start overflow-y-auto h-96">
       <div className="flex items-baseline mb-6 mt-4">
         <h1 className="font-bold text-xl ml-6">لیست کاربران</h1>
         <Search />
       </div>
-      <div className="bg-slate-100 w-11/12 rounded-lg mb-6">
-        <UserCart />
+      <div className="bg-slate-100 w-11/12 rounded-lg mb-6 flex flex-wrap">
+        <UserCart name="سجاد" lastName="جوازی" phone={9121233456} relate="خودم" email="sajjadkiu1@gmail.com" />
       </div>
     </div>
   );
