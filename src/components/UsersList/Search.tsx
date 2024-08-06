f801afef9e740e65bedc8c57d7b31d3870b48577
sajjadjavazi <sajjad.javazi.01@gmail.com>
2024-08-06 23:30:33 +0330

    feat(search): search value handled with useState

--- src/components/UsersList/Search.tsx
+++ src/components/UsersList/Search.tsx
@@ -1,14 +1,29 @@
-const Search = () => {
+import { useState } from "react";
+
+interface SearchProps {
+  onSearch: (value: string) => void;
+}
+
+const Search = ({ onSearch }: SearchProps) => {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleButtonClick = () => {
+    onSearch(inputValue);
+  };
+
   return (
     <div>
       <input
         type="search"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
         className="bg-neutral-200 px-6 py-3 rounded-r-lg"
         placeholder="مخاطب خود را پیدا کنید!"
         aria-label="Search"
         aria-describedby="button-addon3"
       />
       <button
+        onClick={handleButtonClick}
         className="bg-stone-400 hover:bg-stone-500 px-6 py-3 rounded-l-lg"
         data-twe-ripple-init
         data-twe-ripple-color="white"
