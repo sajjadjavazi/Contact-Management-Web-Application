bad13a8042ee392a1ca3035c0554e869143906e9
sajjadjavazi <sajjad.javazi.01@gmail.com>
2024-08-06 19:25:59 +0330

    fix(AddEditUsers): align content changed

--- src/components/AddEditUsers/AddEditUsers.tsx
+++ src/components/AddEditUsers/AddEditUsers.tsx
@@ -1,6 +1,6 @@
 const AddEditUsers = () => {
   return (
-    <div className="flex justify-center w-1/2 flex-wrap">
+    <div className="flex justify-center w-1/2 flex-wrap content-start">
       <h1 className="font-bold text-xl mb-4 mt-4">اضافه / ویرایش کاربران</h1>
       <div className="bg-slate-100 w-11/12 rounded-lg mb-6 py-3">
         <div className="mx-4">
