a9a33384754e3484126b93b5398a0c28ddf9af30
sajjadjavazi <sajjad.javazi.01@gmail.com>
2024-08-06 23:33:35 +0330

    feat(UserCart): id added to the props

--- src/components/UsersList/UserCart/UserCart.tsx
+++ src/components/UsersList/UserCart/UserCart.tsx
@@ -3,13 +3,14 @@ type Cart = {
   lastName: string,
   phone: number,
   relate: string,
-  email: string
+  email: string,
+  id?: number
 }
 
 
-const UserCart = ({name, lastName, phone, relate, email}: Cart) => {
+const UserCart = ({name, lastName, phone, relate, email, id}: Cart) => {
   return (
-    <div className="bg-slate-300 w-5/12 m-4 rounded-xl py-5 px-8">
+    <div className="bg-slate-300 w-5/12 m-4 rounded-xl py-5 px-8" key={id}>
       <div className="mb-1">نام: {name} {lastName}</div>
       <div className="mb-1">شماره تماس: {phone}</div>
       <div className="mb-1">نسبت: {relate}</div>
