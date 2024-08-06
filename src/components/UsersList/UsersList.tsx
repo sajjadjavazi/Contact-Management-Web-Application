e2fc09d5b9c01306ce9d165f8fae2e2665419095
Sajjad <sajjad.javazi.01@gmail.com>
2024-08-06 16:48:25 +0330

    feat(UsersList): search compnent added.

--- src/components/UsersList/UsersList.tsx
+++ src/components/UsersList/UsersList.tsx
@@ -1,11 +1,15 @@
+import Search from "./Search";
 import UserCart from "./UserCart/UserCart";
 
 const UsersList = () => {
   return (
     <div className="flex justify-center w-1/2 flex-wrap content-start">
-      <h1 className="font-bold text-xl mb-4 mt-4">لیست کاربران</h1>
+      <div className="flex items-baseline mb-6 mt-4">
+        <h1 className="font-bold text-xl ml-6">لیست کاربران</h1>
+        <Search />
+      </div>
       <div className="bg-slate-100 w-11/12 rounded-lg mb-6">
-      <UserCart />
+        <UserCart />
       </div>
     </div>
   );
