251a86c26f99c3d3e6d11653d7af0591f56caf33
sajjadjavazi <sajjad.javazi.01@gmail.com>
2024-08-06 23:35:31 +0330

    feat(CartData): cartdata created with four default user init

---
+++ src/data/CartData.tsx
@@ -0,0 +1,34 @@
+export const CartData = [
+    {
+        id: 1,
+        name: "سجاد",
+        lastName: "جوازی",
+        phone: 9121234567,
+        relate: "خودم",
+        email: "sajjadkiu1@gmail.com"
+    },
+    {
+        id: 2,
+        name: "علی",
+        lastName: "محمدی",
+        phone: 9121234567,
+        relate: "دوست",
+        email: "sajjadkiu1@gmail.com"
+    },
+    {
+        id: 3,
+        name: "مصطفی",
+        lastName: "نجفی",
+        phone: 9121234567,
+        relate: "همکار",
+        email: "sajjadkiu1@gmail.com"
+    },
+    {
+        id: 4,
+        name: "مهران",
+        lastName: "ملک زاده",
+        phone: 9121234567,
+        relate: "دوست قدیمی",
+        email: "sajjadkiu1@gmail.com"
+    },
+]
