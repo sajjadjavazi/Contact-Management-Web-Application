9d87cea80f7aa9c16dce3856039ef9f07c0556fb
sajjadjavazi <sajjad.javazi.01@gmail.com>
2024-08-07 20:10:22 +0330

    feat(UserCart): delete action initialized

--- src/components/UsersList/UserCart/UserCart.tsx
+++ src/components/UsersList/UserCart/UserCart.tsx
@@ -1,14 +1,15 @@
 type Cart = {
-  name: string,
-  lastName: string,
-  phone: number,
-  relate: string,
-  email: string,
-  id?: number
-}
+  name: string;
+  lastName: string;
+  phone: number;
+  relate: string;
+  email: string;
+  id?: number;
+  deleteAction:()=> void;
+};
 
 
-const UserCart = ({name, lastName, phone, relate, email, id}: Cart) => {
+const UserCart = ({name, lastName, phone, relate, email, id, deleteAction}: Cart) => {
   return (
     <div className="bg-slate-300 w-5/12 m-4 rounded-xl py-5 px-8" key={id}>
       <div className="mb-1">نام: {name} {lastName}</div>
@@ -17,7 +18,7 @@ const UserCart = ({name, lastName, phone, relate, email, id}: Cart) => {
       <div>ایمیل: {email}</div>
       <div className="flex justify-end mt-4">
         <button className="bg-blue-500 hover:bg-blue-600 py-2 px-4 rounded-tr-md rounded-br-md">ویرایش</button>
-        <button className="bg-red-500 hover:bg-red-600 py-2 px-4 rounded-tl-md rounded-bl-md">حذف</button>
+        <button className="bg-red-500 hover:bg-red-600 py-2 px-4 rounded-tl-md rounded-bl-md" onClick={deleteAction}>حذف</button>
       </div>
     </div>
   );
