88cc52a5b5dfb5679bebc955a16ad576ac189de9
sajjadjavazi <sajjad.javazi.01@gmail.com>
2024-08-07 22:09:36 +0330

    feat(Contact): add and delete user handled

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,16 +1,29 @@
+import { useState } from "react";
 import AddEditUsers from "../components/AddEditUsers/AddEditUsers";
 import UsersList from "../components/UsersList/UsersList";
 import Layout from "../layout/Layout";
+import { CartData } from "../data/CartData";
+import { User } from "../types/User";
 
-const Contact = () => {
+const Contact: React.FC = () => {
+    const [users, setUsers] = useState<User[]>(CartData);
+
+    const handleAddUser = (newUser: User) => {
+        setUsers([...users, newUser]);
+    };
+
+    const handleDeleteUser = (id: number) => {
+        setUsers(users.filter(user => user.id !== id));
+    };
+
     return (
         <Layout>
             <div className="flex justify-between">
-                <AddEditUsers />
-                <UsersList />
+                <AddEditUsers onAddUser={handleAddUser} />
+                <UsersList users={users} onDeleteUser={handleDeleteUser} />
             </div>
         </Layout>
     );
-}
+};
 
 export default Contact;
